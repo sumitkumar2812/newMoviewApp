1c0f0384dd3fbd4bb5a148125f4fb9e30a902e4d
Sumit <sumitkumar7355289198@gmail.com>
2023-05-10 05:24:53 +0000

    MovieApp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+import LoginForm from './components/LoginForm'
+import MoviesHomePage from './components/MoviesHomePage'
+
+function App() {
+  return (
+    <>
+      <LoginForm />
+      <MoviesHomePage />
+    </>
+  )
+}
+export default App
